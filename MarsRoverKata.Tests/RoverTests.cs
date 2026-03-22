97f833ffdbe3fbfd4a76fc3119cfb8a0e5cc5971
Sebastian <sebastiangarciapachano@gmail.com>
2026-03-23 00:08:22 +0100

    add commandsMoveForward and MoveBackward

--- MarsRoverKata.Tests/RoverTests.cs
+++ MarsRoverKata.Tests/RoverTests.cs
@@ -12,4 +12,24 @@ public class RoverTests
         Assert.Equal(new Position(0, 0), rover.Position);
         Assert.Equal(Direction.North, rover.Direction);
     }
+
+    [Fact]
+    public void Moves_forward_when_facing_north()
+    {
+        var rover = new Rover(new Position(0, 0), Direction.North);
+
+        rover.MoveForward();
+
+        Assert.Equal(new Position(0, 1), rover.Position);
+    }
+
+    [Fact]
+    public void Moves_backward_when_facing_north()
+    {
+        var rover = new Rover(new Position(0, 0), Direction.North);
+
+        rover.MoveBackward();
+
+        Assert.Equal(new Position(0, -1), rover.Position);
+    }
 }
--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -2,12 +2,36 @@ namespace MarsRoverKata;
 
 public class Rover
 {
-    public Position Position { get; }
-    public Direction Direction { get; }
+    public Position Position { get; private set; }
+    public Direction Direction { get; private set; }
 
     public Rover(Position position, Direction direction)
     {
         Position = position;
         Direction = direction;
+    }
+
+    public void MoveForward()
+    {
+        Position = Direction switch
+        {
+            Direction.North => new Position(Position.X, Position.Y + 1),
+        Direction.South => new Position(Position.X, Position.Y - 1),
+        Direction.East  => new Position(Position.X + 1, Position.Y),
+        Direction.West  => new Position(Position.X - 1, Position.Y),
+        _ => Position
+        };
+    }
+
+    public void MoveBackward()
+    {
+        Position = Direction switch
+        {
+            Direction.North => new Position(Position.X, Position.Y - 1),
+            Direction.South => new Position(Position.X, Position.Y + 1),
+            Direction.East  => new Position(Position.X - 1, Position.Y),
+            Direction.West  => new Position(Position.X + 1, Position.Y),
+            _ => Position
+        };
     }
 }
