96ce5c04d5c804bc160ced565bc51f6dd2f42fe8
Sebastian <sebastiangarciapachano@gmail.com>
2026-04-01 07:37:33 +0200

    add CommandParser tests for singleton and custom dictionary, implement Singleton pattern in commands and injectable dictionary in parser

--- MarsRoverKata/CommandParser.cs
+++ MarsRoverKata/CommandParser.cs
@@ -2,20 +2,35 @@ namespace MarsRoverKata;
 
 public class CommandParser
 {
-    public IEnumerable<ICommand> Parse(char[] commands)
+    private static readonly Dictionary<char, ICommand> DefaultCommands = new()
     {
-        if (commands == null) throw new ArgumentNullException(nameof(commands));
+        { 'f', MoveForwardCommand.Instance },
+        { 'b', MoveBackwardCommand.Instance },
+        { 'l', TurnLeftCommand.Instance },
+        { 'r', TurnRightCommand.Instance }
+    };
 
-        foreach (var command in commands)
+    private readonly Dictionary<char, ICommand> _commands;
+
+    public CommandParser(Dictionary<char, ICommand>? commands = null)
+    {
+        _commands = commands ?? DefaultCommands;
+    }
+
+    public IEnumerable<ICommand> Parse(char[] input)
+    {
+        if (input == null) throw new ArgumentNullException(nameof(input));
+        return ParseCommands(input);
+    }
+
+    private IEnumerable<ICommand> ParseCommands(char[] input)
+    {
+        foreach (var c in input)
         {
-            yield return command switch
-        {
-            'f' => new MoveForwardCommand(),
-            'b' => new MoveBackwardCommand(),
-            'l' => new TurnLeftCommand(),
-            'r' => new TurnRightCommand(),
-            _ => throw new ArgumentException($"Invalid command '{command}' in command string.")
-        };
+            if (!_commands.TryGetValue(c, out var command))
+                throw new ArgumentException($"Invalid command '{c}'");
+
+            yield return command;
         }
     }
 }
